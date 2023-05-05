a1a982c4cddda3e85739d9a8fba717e5acf866eb
Daniela <danielatomoenk@gmail.com>
2023-05-05 19:17:17 -0300

    adicionando o sort à tabela

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Sort } from '@angular/material/sort';
 
 import { environment } from 'src/environments/enviroment';
 import { IBusiness } from 'src/app/IBusiness';
@@ -12,10 +13,13 @@ import { TableService } from 'src/app/services/table.service';
 })
 export class TableComponent {
   business: IBusiness[] = []
+  sortedData: IBusiness[]
 
   constructor (
     private tableService: TableService
-  ) {}
+  ) {
+    this.sortedData = this.business.slice()
+  }
 
   ngOnInit() {
     this.tableService.getAllData().subscribe(items => {
@@ -24,4 +28,40 @@ export class TableComponent {
   }
 
   displayedColumns: string[] = ['name', 'business', 'valuation', 'active', 'details']
+  
+  formatCurrency(value: number): string {
+    if (isNaN(value)) return '';
+
+    return 'R$ ' + value.toLocaleString('pt-BR', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+      useGrouping: true
+    })
+  }
+
+  sortData(sort: Sort) {
+    const data = this.business.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data
+      return
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch(sort.active) {
+        case 'name':
+          return this.compare(a.name, b.name, isAsc);
+        case 'business':
+          return this.compare(a.business, b.business, isAsc);
+        case 'valuation':
+          return this.compare(a.valuation, b.valuation, isAsc)
+        default:
+          return 0;
+      }
+    })
+  }
+
+  compare(a:string | number, b: string | number, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
 }
